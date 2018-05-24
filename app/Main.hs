c2b5464f24268aa573e91df9c45e517a4500da3e
Islon Scherer <islon.scherer@rewe-digital.com>
2018-05-24 16:58:30 +0200

    basic options parser and purs call

--- app/Main.hs
+++ app/Main.hs
@@ -2,13 +2,46 @@ module Main where
 
 import Prelude.Compat
 
+import qualified Command.Build as Build
+
 import qualified Options.Applicative as Opts
 import qualified System.IO as IO
 import qualified Text.PrettyPrint.ANSI.Leijen as Doc
+
+import Data.Foldable (fold)
+import Data.Monoid ((<>))
 import System.Environment (getArgs)
+
+-- | hardcoded for now
+version :: String
+version = "0.1.0"
 
 main :: IO ()
 main = do
   IO.hSetEncoding IO.stdout IO.utf8
   IO.hSetEncoding IO.stderr IO.utf8
-  putStrLn "Not yet"
+  cmd <- Opts.handleParseResult . execParserPure opts =<< getArgs
+  cmd
+  where
+    opts = Opts.info (versionInfo <*> Opts.helper <*> commands) infoModList
+    infoModList = Opts.fullDesc <> header <> footerInfo
+    header = Opts.progDesc "Purescript -> Clojure backend compiler"
+    footerInfo = Opts.footerDoc (Just footer)
+    footer = mconcat [ Doc.hardline
+                     , Doc.text $ "pursclj " ++ version ]
+
+    versionInfo :: Opts.Parser (a -> a)
+    versionInfo = Opts.abortOption (Opts.InfoMsg version) $
+      Opts.long "version" <> Opts.help "Show the version number" <> Opts.hidden
+
+    execParserPure :: Opts.ParserInfo a -> [String] -> Opts.ParserResult a
+    execParserPure pinfo [] = Opts.Failure $
+      Opts.parserFailure Opts.defaultPrefs pinfo Opts.ShowHelpText mempty
+    execParserPure pinfo args = Opts.execParserPure Opts.defaultPrefs pinfo args
+
+    commands :: Opts.Parser (IO ())
+    commands =
+      (Opts.subparser . fold)
+      [ Opts.command "compile"
+        (Opts.info Build.command
+         (Opts.progDesc "Compile PureScript source files"))]
